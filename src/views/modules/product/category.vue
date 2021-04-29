50dcd0b9f7ea5536b833b07536a20321e4866f9d
xiaolei <1626053234@qq.com>
2021-04-29 14:19:26 +0800

    feat:项目初始化

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,32 @@
+<template>
+ <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+</template>
+
+<script>
+export default {
+data() {
+      return {
+        menus: []
+    };
+    
+  },
+  methods: {
+      // 获取数据列表
+      getMenus () {
+        this.$http({
+          url: this.$http.adornUrl('/product/category/list/tree'),
+          method: 'get'
+        }).then(({data}) => {
+          this.menus = data.data;
+        })
+      },
+    },
+    created() {
+      this.getMenus();
+    },
+}
+</script>
+
+<style>
+
+</style>

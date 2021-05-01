ea6fa9b11cae1b2a81a0b6c7ecb99735f46d41bb
xiaolei <1626053234@qq.com>
2021-05-01 22:52:38 +0800

    feat:新增分类删除功能

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,32 +1,100 @@
 <template>
- <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <el-tree
+    :data="menus"
+    show-checkbox
+    :props="defaultProps"
+    :expand-on-click-node="false"
+    node-key="catId"
+    :default-expanded-keys="expandedKey"
+  >
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+      <span>{{ node.label }}</span>
+      <span>
+        <el-button
+          v-if="node.level <= 2"
+          type="text"
+          size="mini"
+          @click="() => append(data)"
+        >
+          增加子菜单
+        </el-button>
+        <el-button
+          v-if="node.childNodes.length == 0"
+          type="text"
+          size="mini"
+          @click="() => remove(node, data)"
+        >
+          删除
+        </el-button>
+      </span>
+    </span>
+  </el-tree>
 </template>
 
 <script>
 export default {
-data() {
-      return {
-        menus: []
+  data() {
+    return {
+      menus: [],
+      expandedKey:[],
+      defaultProps: {
+        children: "children",
+        label: "name",
+      },
     };
-    
   },
   methods: {
-      // 获取数据列表
-      getMenus () {
-        this.$http({
-          url: this.$http.adornUrl('/product/category/list/tree'),
-          method: 'get'
-        }).then(({data}) => {
-          this.menus = data.data;
+    // 获取数据列表
+    getMenus() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get",
+      }).then(({ data }) => {
+        this.menus = data.data;
+      });
+    },
+    append(data) {
+      console.log(data);
+    },
+    remove(node, data) {
+      var ids = [data.catId];
+      // 弹窗 确认
+      this.$confirm(`是否删除【${data.name}】菜单?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          // 点击确定
+          this.$http({
+            // 给delete发送
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(ids, false),
+          }).then(({ data }) => {
+            // 删除成功$message
+            this.$message({
+              message: "菜单删除成功",
+              type: "success",
+            });
+            //刷新出新的菜单
+            this.getMenus();
+            //设置需要默认展开的菜单
+            this.expandedKey = [node.parent.data.catId];
+          });
         })
-      },
+        .catch(
+          // 取消
+          () => {}
+        );
     },
-    created() {
-      this.getMenus();
-    },
-}
+    handleNodeClick() {},
+  },
+  created() {
+    this.getMenus();
+  },
+};
 </script>
 
 <style>
-
 </style>
